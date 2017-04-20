f222751a1d749a5580a3268e20e3c428504ade94
kamonlphop <kamonlphop_bkk@hotmail.com>
2017-04-19 21:47:50 -0700

    Test Map

--- app/src/main/java/comsci/kamonlphop/connectmap/CallMap.java
+++ app/src/main/java/comsci/kamonlphop/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package comsci.kamonlphop.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -10,6 +14,24 @@ public class CallMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
 
-    }//end maincall
 
+
+
+        }//end maincall
+        public void runCallMap(View view){
+
+            EditText latitude = (EditText) findViewById(R.id.mapLat);
+            EditText longtutude = (EditText) findViewById(R.id.mayLong);
+            EditText labell = (EditText) findViewById(R.id.mapLabel);
+
+            String lat = latitude.getText().toString().trim();
+            String longti = longtutude.getText().toString().trim();
+            String mlabell = labell.getText().toString().trim();
+
+            Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.759764,100.541532(ศูยน์รางน้ำ)");//link googlemap
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
+            //mapIntent.setPackage("com.google.andriod.app.maps");//ใช้งานกับโทรศัพท์มือถืือ(ใช้งานกับgooglemap โดยตรง)
+            startActivity(mapIntent);
+
+        }//end runcallmap
 }//end class
